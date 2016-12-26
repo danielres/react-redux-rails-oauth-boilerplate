b8cb7f4c23276ae9f471f1b8edc23bab54905a67
Daniel Reszka <dannn.r@gmail.com>
2016-12-26 03:12:21 +0100

    use a hash for action types

--- frontend/src/TodoList/actions.js
+++ frontend/src/TodoList/actions.js
@@ -2,45 +2,43 @@ import config from '../config.js'
 import axios from 'axios'
 import store from '../store'
 
-export const LIST_REQUEST = 'TodoList/list/request'
-export const LIST_SUCCESS = 'TodoList/list/success'
-export const LIST_FAILURE = 'TodoList/list/failure'
-
-export const NEW_UPDATE  = 'TodoList/NewItem/update'
-
-export const CREATE_REQUEST = 'TodoList/Item/create/request'
-export const CREATE_SUCCESS = 'TodoList/Item/create/success'
-export const CREATE_FAILURE = 'TodoList/Item/create/failure'
-
-export const DESTROY_REQUEST = 'TodoList/Item/destroy/request'
-export const DESTROY_SUCCESS = 'TodoList/Item/destroy/success'
-export const DESTROY_FAILURE = 'TodoList/Item/destroy/failure'
-
-export const UPDATE_REQUEST = 'TodoList/Item/update/request'
-export const UPDATE_SUCCESS = 'TodoList/Item/update/success'
-export const UPDATE_FAILURE = 'TodoList/Item/update/failure'
+export const types = {
+  LIST_REQUEST:      'TodoList/list/request',
+  LIST_SUCCESS:      'TodoList/list/success',
+  LIST_FAILURE:      'TodoList/list/failure',
+  NEW_UPDATE:        'TodoList/NewItem/update',
+  CREATE_REQUEST:    'TodoList/Item/create/request',
+  CREATE_SUCCESS:    'TodoList/Item/create/success',
+  CREATE_FAILURE:    'TodoList/Item/create/failure',
+  DESTROY_REQUEST:   'TodoList/Item/destroy/request',
+  DESTROY_SUCCESS:   'TodoList/Item/destroy/success',
+  DESTROY_FAILURE:   'TodoList/Item/destroy/failure',
+  UPDATE_REQUEST:    'TodoList/Item/update/request',
+  UPDATE_SUCCESS:    'TodoList/Item/update/success',
+  UPDATE_FAILURE:    'TodoList/Item/update/failure',
+}
 
 export const fetchTodoListItems = () => {
-  store.dispatch({type: LIST_REQUEST})
+  store.dispatch({type: types.LIST_REQUEST})
   axios.get(`${config.API_ENDPOINT}/items`)
     .then(({data}) => {
-      store.dispatch({type: LIST_SUCCESS, data})
+      store.dispatch({type: types.LIST_SUCCESS, data})
     })
     .catch((error) => {
-      store.dispatch({type: LIST_FAILURE, error})
+      store.dispatch({type: types.LIST_FAILURE, error})
     })
 }
 
 export const updateNewItem = (values) => ({
-  type: NEW_UPDATE,
+  type: types.NEW_UPDATE,
   values,
 })
 
 export const createItem = (params) => {
-  store.dispatch({type: CREATE_REQUEST, params})
+  store.dispatch({type: types.CREATE_REQUEST, params})
   axios.post(`${config.API_ENDPOINT}/items`, params)
   .then(({data}) => {
-    store.dispatch({type: CREATE_SUCCESS, data})
+    store.dispatch({type: types.CREATE_SUCCESS, data})
   })
   .catch((error) => {
     console.log(error)
@@ -49,10 +47,10 @@ export const createItem = (params) => {
 }
 
 export const destroyItem = (id) => {
-  store.dispatch({type: DESTROY_REQUEST, id})
+  store.dispatch({type: types.DESTROY_REQUEST, id})
   axios.delete(`${config.API_ENDPOINT}/items/${id}`)
   .then(({data}) => {
-    store.dispatch({type: DESTROY_SUCCESS, data})
+    store.dispatch({type: types.DESTROY_SUCCESS, data})
   })
   .catch((error) => {
     console.log(error)
@@ -62,10 +60,10 @@ export const destroyItem = (id) => {
 
 
 export const updateItem = (id, params) => {
-  store.dispatch({type: UPDATE_REQUEST, id, params})
+  store.dispatch({type: types.UPDATE_REQUEST, id, params})
   axios.put(`${config.API_ENDPOINT}/items/${id}`, params)
   .then(({data}) => {
-    store.dispatch({type: UPDATE_SUCCESS, data})
+    store.dispatch({type: types.UPDATE_SUCCESS, data})
   })
   .catch((error) => {
     console.log({error})
