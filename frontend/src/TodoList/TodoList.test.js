86153ef88a36a6705cc83f12a09a30c0f9508218
Daniel Reszka <dannn.r@gmail.com>
2016-12-29 09:43:29 +0100

    add TodoList.test.js with a snapshot test

---
+++ frontend/src/TodoList/TodoList.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import TodoList from './TodoList'
+
+const fetchTodoListItems = () => {}
+const data = [{id: 1, name: "one"}]
+const NewItem = {name: "hello world"}
+const props = {fetchTodoListItems, data, NewItem}
+const component = renderer.create(<TodoList {...props} />)
+const tree = component.toJSON()
+
+it('matches snapshot', () => {
+  expect(tree).toMatchSnapshot()
+})
