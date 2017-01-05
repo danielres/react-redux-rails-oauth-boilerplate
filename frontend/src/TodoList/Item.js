bc8af007419d35c6eb0e12e0682c706a92a5c3ff
Daniel Reszka <dannn.r@gmail.com>
2017-01-05 14:21:57 +0100

    turn Item into a functional component

--- frontend/src/TodoList/Item.js
+++ frontend/src/TodoList/Item.js
@@ -1,43 +1,43 @@
-import React, { Component } from 'react'
+import React from 'react'
+// TODO: add proptypes
+export default function (props) {
+  const {
+    item,
+    destroyItem,
+    updateItem,
+  } = props
 
-export default class extends Component {
-  render() {
-    const {item} = this.props
-    const {destroyItem} = this.props
-    const {updateItem} = this.props
+  const toggleCompleted = () =>
+    updateItem(item.id, {completed: !item.completed})
 
-    const toggleCompleted = () =>
-      updateItem(item.id, {completed: !item.completed})
-
-    const className = (i) => {
-      var c = []
-      c.push("TodoList-Item")
-      if (i.completed) c.push("TodoList-Item--completed")
-      return c.join(" ")
-    }
-
-    return (
-      <li
-        className={className(item)}
-      >
-        <button
-          className="TodoList-Item-DestroyButton"
-          onClick={() => destroyItem(item.id)}
-        >
-          X
-        </button>
-
-        <button
-          className="TodoList-Item-ToggleCompletedButton"
-          onClick={toggleCompleted}
-        >
-          V
-        </button>
-
-        <div>
-          {item.name}
-        </div>
-      </li>
-    )
+  const className = (i) => {
+    var c = []
+    c.push("TodoList-Item")
+    if (i.completed) c.push("TodoList-Item--completed")
+    return c.join(" ")
   }
+
+  return (
+    <li
+      className={className(item)}
+    >
+      <button
+        className="TodoList-Item-DestroyButton"
+        onClick={() => destroyItem(item.id)}
+      >
+        X
+      </button>
+
+      <button
+        className="TodoList-Item-ToggleCompletedButton"
+        onClick={toggleCompleted}
+      >
+        V
+      </button>
+
+      <div>
+        {item.name}
+      </div>
+    </li>
+  )
 }
