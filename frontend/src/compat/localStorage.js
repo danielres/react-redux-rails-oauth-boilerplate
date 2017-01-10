31dc017dd83fc3ea8e9a739260b3cddd07eacc81
Daniel Reszka <dannn.r@gmail.com>
2017-01-10 17:15:54 +0100

    remove node-localstorage integration

--- frontend/src/compat/localStorage.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-
-if (typeof localStorage === "undefined" || localStorage === null) {
-  var LocalStorage = require('node-localstorage').LocalStorage
-  var shim = new LocalStorage('./scratch')
-}
-export default shim || localStorage
