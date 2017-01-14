ea39d0e3384eaf4dfec6ad48d9017ec822737581
Daniel Reszka <dannn.r@gmail.com>
2017-01-14 08:41:30 +0100

    config active_record to use uuid as primary_key_type

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -21,5 +21,8 @@ module Todolist1
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
