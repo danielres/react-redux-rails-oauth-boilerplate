02844b073442c8bbb7874cbf1f864cebf4b9b31d
Daniel Reszka <dannn.r@gmail.com>
2017-01-18 14:33:58 +0100

    add test coverage for User #access_token and #find_by_access_token

--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -6,6 +6,19 @@ RSpec.describe User, type: :model do
   it do
     expect(user).to be_valid
   end
+
+  describe "#access_token" do
+    it "works" do
+      expect(user.access_token.length > 100).to eq true
+    end
+  end
+
+  describe "#find_by_access_token" do
+    let(:access_token){ user.access_token}
+    it "works" do
+      expect( User.find_by_access_token(access_token) ).to eq(user)
+    end
+  end
 end
 
 
