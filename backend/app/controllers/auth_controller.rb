62efc4c12057b9ca8fb5e67491ac614d43a48f10
Daniel Reszka <dannn.r@gmail.com>
2017-01-18 08:14:58 +0100

    fix auth_controller#auth, return access_token upon user authentication

--- backend/app/controllers/auth_controller.rb
+++ backend/app/controllers/auth_controller.rb
@@ -2,7 +2,10 @@ class AuthController < ApplicationController
   protect_from_forgery except: :auth
 
   def auth
-    run User::FindOrCreateByProvider
-    render json: UserRepresenter.new(@model)
+    result                = run User::Authenticate
+    access_token, model   = result['access_token'], result['model']
+    model_hash            = JSON.parse(UserRepresenter.new(model).to_json)
+    access_token_hash     = {access_token: access_token}
+    render json: model_hash.merge(access_token_hash)
   end
 end
