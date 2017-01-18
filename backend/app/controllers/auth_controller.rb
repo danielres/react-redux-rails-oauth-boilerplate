9df45d5115ee7c7ce0ac93aaa78693881e75e49c
Daniel Reszka <dannn.r@gmail.com>
2017-01-18 11:34:39 +0100

    code cleanup

--- backend/app/controllers/auth_controller.rb
+++ backend/app/controllers/auth_controller.rb
@@ -2,10 +2,10 @@ class AuthController < ApplicationController
   protect_from_forgery except: :auth
 
   def auth
-    result                = run User::Authenticate
-    model                 = result['model']
-    model_hash            = JSON.parse(UserRepresenter.new(model).to_json)
-    access_token_hash     = {access_token: model.access_token}
+    result            = run User::Authenticate
+    model             = result['model']
+    model_hash        = JSON.parse(UserRepresenter.new(model).to_json)
+    access_token_hash = {access_token: model.access_token}
     render json: model_hash.merge(access_token_hash)
   end
 end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   def self.find_by_access_token(access_token)
     decoded = JsonWebToken.decode(access_token)
     return nil unless decoded
-    User.find(decoded['user_id']) rescue nil
+    find(decoded['user_id']) rescue nil
   end
 
   def access_token
