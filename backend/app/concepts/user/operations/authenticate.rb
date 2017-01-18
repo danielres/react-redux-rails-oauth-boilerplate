615886f68c4ed708c443a0c7c48a3c4d0e143be7
Daniel Reszka <dannn.r@gmail.com>
2017-01-18 11:16:38 +0100

    extract #find_by_access_token and #access_token from operation to User

--- backend/app/concepts/user/operations/authenticate.rb
+++ backend/app/concepts/user/operations/authenticate.rb
@@ -1,15 +1,10 @@
 class User::Authenticate < Trailblazer::Operation
-  success :find_by_token
+  success :find_by_access_token
   success :find_or_create_by_provider
   success :define_access_token
 
-  def find_by_token(options)
-    access_token = JsonWebToken.decode(options['params']['access_token'])
-    return unless access_token
-    user_id = access_token['user_id']
-    user = User.find(user_id) rescue nil
-    return unless user
-    options['model'] = user
+  def find_by_access_token(options)
+    options['model'] = User.find_by_access_token(options['params']['access_token'])
   end
 
   def find_or_create_by_provider(options)
@@ -19,7 +14,6 @@ class User::Authenticate < Trailblazer::Operation
   end
 
   def define_access_token(options)
-    user_id = options['model'].id
-    options['access_token'] = JsonWebToken.encode({user_id: user_id})
+    options['access_token'] = options['model'].access_token
   end
 end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  def self.find_by_access_token(access_token)
+    decoded = JsonWebToken.decode(access_token)
+    return nil unless decoded
+    User.find(decoded['user_id']) rescue nil
+  end
+
+  def access_token
+    JsonWebToken.encode({user_id: id})
+  end
 end
