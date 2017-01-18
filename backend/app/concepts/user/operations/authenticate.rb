4991467447e93ccba9856b80a80d9e61441bee03
Daniel Reszka <dannn.r@gmail.com>
2017-01-18 11:27:07 +0100

    remove define_access_token from operation (moved to User#access_token)

--- backend/app/concepts/user/operations/authenticate.rb
+++ backend/app/concepts/user/operations/authenticate.rb
@@ -1,7 +1,6 @@
 class User::Authenticate < Trailblazer::Operation
   success :find_by_access_token
   success :find_or_create_by_provider
-  success :define_access_token
 
   def find_by_access_token(options)
     options['model'] = User.find_by_access_token(options['params']['access_token'])
@@ -11,9 +10,5 @@ class User::Authenticate < Trailblazer::Operation
     return if options['model']
     result = User::FindOrCreateByProvider.(options['params'])
     options['model'] = result['model']
-  end
-
-  def define_access_token(options)
-    options['access_token'] = options['model'].access_token
   end
 end
--- backend/app/controllers/auth_controller.rb
+++ backend/app/controllers/auth_controller.rb
@@ -3,9 +3,9 @@ class AuthController < ApplicationController
 
   def auth
     result                = run User::Authenticate
-    access_token, model   = result['access_token'], result['model']
+    model                 = result['model']
     model_hash            = JSON.parse(UserRepresenter.new(model).to_json)
-    access_token_hash     = {access_token: access_token}
+    access_token_hash     = {access_token: model.access_token}
     render json: model_hash.merge(access_token_hash)
   end
 end
