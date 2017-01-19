c893536f84e3949e37a7d0ef8ef82e4ace5d3684
Daniel Reszka <dannn.r@gmail.com>
2017-01-19 14:01:41 +0100

    fic apiMockAdapter always enabled

--- frontend/src/apiMockAdapter.js
+++ frontend/src/apiMockAdapter.js
@@ -1,4 +1,7 @@
 import axios from 'axios'
 import Adapter from 'axios-mock-adapter'
+import config from './config.js'
 
-export default new Adapter(axios)
+export default config.mockApis
+  ? new Adapter(axios)
+  : () => {}
