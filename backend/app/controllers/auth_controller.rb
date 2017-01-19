2944b31823addeb754eb684593c011d1e516b058
Daniel Reszka <dannn.r@gmail.com>
2017-01-19 15:06:45 +0100

    rename access_token to accessToken in API

--- backend/app/controllers/auth_controller.rb
+++ backend/app/controllers/auth_controller.rb
@@ -5,7 +5,7 @@ class AuthController < ApplicationController
     result            = run User::Authenticate
     model             = result['model']
     model_hash        = JSON.parse(UserRepresenter.new(model).to_json)
-    access_token_hash = {access_token: model.access_token}
+    access_token_hash = {accessToken: model.access_token}
     render json: model_hash.merge(access_token_hash)
   end
 end
