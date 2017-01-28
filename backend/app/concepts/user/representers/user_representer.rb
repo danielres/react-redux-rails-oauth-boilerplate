8025c69ff6100792a10fc1c797d346d4f9abb4ea
Daniel Reszka <dannn.r@gmail.com>
2017-01-28 06:50:52 +0100

    delete whole auth layer

--- backend/app/concepts/user/representers/user_representer.rb
+++
@@ -1,8 +0,0 @@
-class UserRepresenter < Representable::Decorator
-  include Representable::JSON
-  property :id
-  property :profile
-  property :inviter_id
-  property :created_at
-  property :updated_at
-end
--- backend/app/controllers/auth_controller.rb
+++ backend/app/controllers/auth_controller.rb
@@ -1,11 +1,4 @@
 class AuthController < ApplicationController
   protect_from_forgery except: :auth
 
-  def auth
-    result            = run User::Authenticate
-    model             = result['model']
-    model_hash        = JSON.parse(UserRepresenter.new(model).to_json)
-    access_token_hash = {accessToken: model.access_token}
-    render json: model_hash.merge(access_token_hash)
-  end
 end
