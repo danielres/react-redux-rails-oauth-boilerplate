5f52f6a5b243ca2bc665a5157f3e09735a1ea10e
Daniel Reszka <dannn.r@gmail.com>
2017-01-30 20:29:59 +0100

    setup travis with ruby and node

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['CORS_ORIGINS']
+    origins ENV['CORS_ORIGINS'] || ''
     resource '*',
       headers: :any,
       methods: %i(get post put patch delete options head)
